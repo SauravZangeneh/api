be775c7eda787c0261adfd6178b2a020342f2798
SauravZangeneh <saurav.z@outlook.com>
2019-01-23 20:16:04 +0000

    Add files via upload
    added date-time functionality + post method

--- code.py
+++ code.py
@@ -1,17 +1,22 @@
 import flask
 from flask import request, jsonify
 import urllib 
+import datetime
+import time
+
 app = flask.Flask(__name__)
 
 #test document (since documents are just text) 
 #max length (50 chars) - useful for the post method
 #content is stored as values for timestamps (keys)
+
+
 docs ={'Test 1': 
-    	{'1pm': 'oasidn','3pm': 'ajoj'},
+    	{datetime.datetime(2018, 1, 23, 20, 14, 8, 603536): 'oasidn',datetime.datetime(2013, 1, 23, 20, 14, 8, 603536): 'ajoj'},
     'Test 2': 
-    	{'4pm': 'abd','5pm': 'anlo'},
+    	{datetime.datetime(2018, 1, 23, 20, 14, 8, 603536):'asodn'},
     'Test 3': 
-    	{'12am': 'snoan','6pm': 'ando'}
+    	{datetime.datetime(2019, 1, 23, 20, 14, 8, 603536): 'snoan'}
     }
 
 
@@ -26,9 +31,13 @@ def get_titles():
 	return jsonify(docs.keys())
 
 #task 2 - get all timestamps for a title
-@app.route('/documents/<title>', methods=['GET'])
+@app.route('/documents/<title>', methods=['GET','POST'])
 def get_timestamp(title):
+	content = request.json
 	title=title.decode('utf8').replace("+"," ") #basic url parsing
+	if content:
+		docs[title][datetime.datetime.now()]=content['content']
+		print(docs)
 	if title in docs:
 		return jsonify(docs[title].keys())
 	else:
@@ -41,9 +50,12 @@ def get_content(title,timestamp):
 	title=title.decode('utf8').replace("+"," ") #basic url parsing
 	if title in docs:
 		doc=docs[title]
+		timestamp=datetime.fromtimestamp(timestamp)
+		print(timestamp)
 		if timestamp in doc:
 			return doc[timestamp]
 	else:
 		return ("Error doc doesn't exist")
+
 app.run()
 
